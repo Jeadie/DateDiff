28b84c26724c3f817719c8b5e6759ca96d081163
Jack Eadie <jeadi@amazon.com>
2022-03-26 17:09:03 +1000

    add basic Gin server

--- server.go
+++ server.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
+const DefaultPort = uint(8000)
+
 
 func Server(args []string) {
-	router := gin.Default()
-	fmt.Printf("%s\n", router.Routes())
+
+	argFs := flag.NewFlagSet(string(ServerCmd), flag.ExitOnError)
+	port := argFs.Uint("p", DefaultPort, "server connection port")
+	argFs.Parse(args)
+
+	gin.Default().Run(fmt.Sprintf(":%d", *port))
 }
