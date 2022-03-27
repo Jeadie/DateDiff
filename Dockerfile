29f15a06a34f19cea238a59e1c7019bc7f9eb4da
Jeadie <jackeadie@duck.com>
2022-03-27 11:13:56 +1000

    correct the use of port from environment variable in code and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM scratch
 
 MAINTAINER @Jeadie
 
-COPY datediff_docker main
+COPY datediff_docker /main
 
-ENV GIN_MODE=release
+#ENV GIN_MODE=release
 EXPOSE $PORT
-ENTRYPOINT ["/main", "server", "-p", "$PORT"]
+CMD ["/main", "server"]
--- server.go
+++ server.go
@@ -3,16 +3,29 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/Jeadie/DateDiff/diff"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
-const DefaultPort = uint(8000)
+func getDefaultPort() string {
+	value, isSet := os.LookupEnv("PORT")
+	if !isSet || len(value) == 0 {
+		return "8000"
+	}
+	return value
+}
 
 func Server(args []string) {
 
 	argFs := flag.NewFlagSet(string(ServerCmd), flag.ExitOnError)
-	port := argFs.Uint("p", DefaultPort, "server connection port")
+	port := argFs.String("p", getDefaultPort(), "server connection port")
 	argFs.Parse(args)
 
-	gin.Default().Run(fmt.Sprintf(":%d", *port))
+	_, err := diff.UintParse(*port); if err != nil {
+		fmt.Fprintf(os.Stderr, "invalid port specified: %s\n", *port)
+		return
+	}
+
+	gin.Default().Run(fmt.Sprintf(":%s", *port))
 }
